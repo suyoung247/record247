7e511c6b650d302eab99713fe2d5956abe171a44
= <bosoco@naver.com>
2025-06-30 22:01:44 +0900

    Chore: DB  변수 추가 및 의미있는 변수명으로 수정

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,6 +9,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const firebaseAuth = getAuth(app);
+const firestoreDatabase = getFirestore(app);
 
-export { auth };
+export { firebaseAuth, firestoreDatabase };
