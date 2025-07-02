fb4779df69d225dfd05603bdc110cb9d47740d2f
= <bosoco@naver.com>
2025-07-02 20:21:58 +0900

    Refactor: useStore 리팩토링 (highlightColor, modal)

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -2,23 +2,31 @@ import { create } from 'zustand';
 
 const useStore = create((set) => ({
   selectedText: '',
+  selectedNote: null,
   isSettingsOpen: false,
   isModalOpen: false,
+  noteModal: null,
+
   setSelectedText: (text) => set({ selectedText: text }),
   clearSelectedText: () => set({ selectedText: '' }),
 
   openSettings: () => set({ isSettingsOpen: true }),
   closeSettings: () => set({ isSettingsOpen: false }),
 
-  openModal: () => set({ isModalOpen: true }),
-  closeModal: () => set({ isModalOpen: false }),
+  openNoteModal: (note) => set({ noteModal: note, isModalOpen: true }),
+  closeModal: () => set({ noteModal: null, isModalOpen: false }),
 
   reset: () =>
     set({
       selectedText: '',
+      selectedNote: null,
       isSettingsOpen: false,
       isModalOpen: false,
+      noteModal: null,
     }),
+
+  highlightColor: null,
+  setHighlightColor: (color) => set({ highlightColor: color }),
 }));
 
 export default useStore;
